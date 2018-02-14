63c53824fe8623389f46d42cd6419e0ecc1014f5
MauroDiamantino <maurogabriel.54@gmail.com>
2018-02-14 20:14:57 -0300

    Desarrollo de los controladores PID de ambos motores (no probado)
    Signed-off-by: MauroDiamantino <maurogabriel.54@gmail.com>

--- Laberinto/Laberinto.ino
+++ Laberinto/Laberinto.ino
@@ -1,27 +1,6 @@
-//////////////////////////////////////////LIBRERIAS//////////////////////////////////////////////////////////////////////////////////////////////////
-#include <FlexiTimer2.h>
-//#include <TimerOne.h>
-
-//////////////////////////////////////////CONSTANTES//////////////////////////////////////////////////////////////////////////////////////////////////
-//Pines
-const unsigned int PIN_ENCODER_LEFT = 2;
-const unsigned int PIN_ENCODER_RIGHT = 3;
-
-//Constantes del algoritmo
-//const float PI = 3.14; //Es innecesario definir esta constante porque ya esta predefinida
-const float INT_MUESTREO = 1000.0; //Intervalo de muestro en milisegundos
-const unsigned int NUM_FLANCOS_VUELTA = 16; //Cantidad de flancos efectivos por vuelta, es decir flancos que producen interrupciones.
-                                            //Esto depende de la configuracion de las interrupciones del micro.
+#include "Laberinto.h"
 
 //////////////////////////////////////////VARIABLES GLOBALES//////////////////////////////////////////////////////////////////////////////////////////
-//velocidades angulares de las ruedas
-float vel_angular_left = 0; //no se obtiene mejora si se usa el tipo de dato 'double' por la placa que se esta usando
-float vel_angular_right = 0;
-
-//velocidades lineales de las ruedas 
-float vel_lineal_left = 0;
-float vel_lineal_right = 0;
-
 //Contadores de los flancos efectivos (aquellos que producen interrupciones) de los encoder.
 unsigned int flancos_left = 0;
 unsigned int flancos_right = 0;
@@ -30,29 +9,50 @@ unsigned int flancos_right = 0;
 unsigned int total_flancos_left = 0;  
 unsigned int total_flancos_right = 0;
 
+//Parametros del PID del lazo de control L y referencia
+float kp_left = 0.0, ki_left = 0.0, td_left = 0.0; //Parametros del PID
+float r_left = 0.0; //velocidad lineal de referencia (m/s)
+                      
+//Parametros del PID del lazo de control L y referencia
+float kp_right = 0.0, ki_right = 0.0, td_right = 0.0; //Parametros del PID
+float r_right = 0.0; //velocidad lineal de referencia (m/s)
+
+//Buffer de la comunicacion serie
 char buffer[100];
 
 ///////////////////////////////////////////////FLAGs////////////////////////////////////////////////////////////////////////////////////////////////////
-boolean flag_calc_vel = false;
+boolean flag_control = false;
 
 ///////////////////////////////////////////////SETUP////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup(){
-  //Configuración de pines
+  
+  //Configuración del modo de funcionamiento de los pines
   pinMode(PIN_ENCODER_LEFT, INPUT_PULLUP);
   pinMode(PIN_ENCODER_RIGHT, INPUT_PULLUP);
+  pinMode(PIN1_MOTOR_LEFT, OUTPUT);
+  pinMode(PIN2_MOTOR_LEFT, OUTPUT);
+  pinMode(PIN1_MOTOR_RIGHT, OUTPUT);
+  pinMode(PIN2_MOTOR_RIGHT, OUTPUT);
 
+  //Configuracion de la frecuencia de las salidas PWM
+  setPwmFrequency(PIN1_MOTOR_LEFT, 8); //se configura la freq PWM en 8KHz aprox
+  setPwmFrequency(PIN2_MOTOR_LEFT, 8); //se configura la freq PWM en 8KHz aprox
+  setPwmFrequency(PIN1_MOTOR_RIGHT, 8); //se configura la freq PWM en 4KHz aprox
+  setPwmFrequency(PIN2_MOTOR_RIGHT, 8); //se configura la freq PWM en 4KHz aprox
+  //No es posible configurar las frecuencias PWM de ambos motores en el mismo valor
+  //porque las frecuencias base de los pines son diferentes. Sin embargo, lo importante es
+  //que las frecuencias sean suficientemente altas, no iguales.
+  
   //Configuracion de la comunicación serie
-  Serial.begin(9600);
-  Serial.println("DATOS DE LA RUEDA IZQUIERDA");
-  Serial.println("FLANCOS     VEL_ANG     VEL_LIN");
-
+  Serial.begin(VEL_COM_SERIE);
+  
   //Configuracion de las interrupciones
   //Usar CHANGE si se desea que ambos flancos, negativo y positivo, produzcan interrupciones (sean efectivos), y configurar el numero de flancos efectivos
   //por vuelta (NUM_FLANCOS_VUELTA) en 16. Si se usa solo uno de los flancos, usar FALLING o RISING y 8 flancos efectivos por vuelta.
   attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_LEFT),RS_ENCODER_LEFT, CHANGE); 
   attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_RIGHT),RS_ENCODER_RIGHT, CHANGE);
 
-  //Configuración del Timer 2
+  //Configuración del Timer 2, que es utilizado para contar el intervalo de muestreo.
   FlexiTimer2::set(INT_MUESTREO, RS_TIMER2); // El tiempo de muestreo se coloca en milisegundos
   FlexiTimer2::start();
 }
@@ -60,31 +60,16 @@ void setup(){
 ///////////////////////////////////////////////LOOP////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop(){
   
-  if (flag_calc_vel == true){
+  if (flag_control == true){
 
-    vel_angular_left = ( (total_flancos_left/NUM_FLANCOS_VUELTA)*2.0*PI / (INT_MUESTREO/1000.0) );// La velocidad angular esta en rad/s. Se divide el intervalo de 
-                                                                                                //muestreo en 1000 porque esta en milisegundos
-    vel_lineal_left = vel_angular_left*0.03; // La velocidad lineal esta en m/s, se obtiene al multiplicar la velocidad angular por el radio de la rueda (3cm, es decir 0.03m)
-
-    vel_angular_right = ( (total_flancos_right/NUM_FLANCOS_VUELTA)*2.0*PI / (INT_MUESTREO/1000.0) );// La velocidad angular esta en rad/s. Se divide el intervalo de 
-                                                                                                //muestreo en 1000 porque esta en milisegundos
-    vel_lineal_right = vel_angular_right*0.03; // La velocidad lineal esta en m/s, se obtiene al multiplicar la velocidad angular por el radio de la rueda (3cm)
+    ControlMotorLeft(r_left);
+    ControlMotorRight(r_right);
     
-    //sprintf(buffer,"%d            %d           %d",total_flancos_left,vel_angular_left,vel_lineal_left);
-
-    //Exteriorizacion de datos para probar el codigo
-    Serial.print(total_flancos_left);
-    Serial.print("\t\t");
-    Serial.print(vel_angular_left);
-    Serial.print("\t\t");
-    Serial.println(vel_lineal_left);
-     
-    flag_calc_vel = false;
+    flag_control = false;
   }
-
 }
 
-/////////////////////////////////////////////RUTINAS DE SERVICIO/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////RUTINAS DE SERVICIO/////////////////////////////////////////////////////////////////////////////////////////////////
 void RS_ENCODER_LEFT() {    
   flancos_left++ ;
 }
@@ -100,7 +85,161 @@ void RS_TIMER2() {
   flancos_left = 0;
   flancos_right = 0;
 
-  flag_calc_vel = true;
+  flag_control = true;
+}
+
+////////////////////////////////////////////////////////////////////FUNCIONES///////////////////////////////////////////////////////////////////////////////////////////////////
+inline float CalcVelLeft(){
+  float vel_angular_left = 0.0; //velocidad angular de las rueda izquierda
+  float vel_lineal_left = 0.0; //velocidad lineal de la rueda izquierda
+  //no se obtiene mejora si se usa el tipo de dato 'double' por la placa que se esta usando
+  
+  //Calculo de la velocidad lineal del motor izquierdo. Esta es la variable a controlar en el lazo de control L (y_left).
+  vel_angular_left = ( (total_flancos_left/NUM_FLANCOS_VUELTA)*2.0*PI / (INT_MUESTREO/1000.0) );// La velocidad angular esta en rad/s. Se divide el intervalo de 
+                                                                                              //muestreo en 1000 porque esta en milisegundos
+  vel_lineal_left = vel_angular_left*0.03; // La velocidad lineal esta en m/s, se obtiene al multiplicar la velocidad angular por el radio de la rueda (3cm, es decir 0.03m)
+  
+  return vel_lineal_left;
+}
+
+
+inline float CalcVelRight(){
+  float vel_angular_right = 0.0; //velocidad angular de las rueda derecha
+  float vel_lineal_right = 0.0; //velocidad lineal de la rueda derecha
+  //no se obtiene mejora si se usa el tipo de dato 'double' por la placa que se esta usando
+
+  //Calculo de la velocidad lineal del motor derecho. Esta es la variable a controlar en el lazo de control R (y_right).
+  vel_angular_right = ( (total_flancos_right/NUM_FLANCOS_VUELTA)*2.0*PI / (INT_MUESTREO/1000.0) );// La velocidad angular esta en rad/s. Se divide el intervalo de 
+                                                                                              //muestreo en 1000 porque esta en milisegundos
+  vel_lineal_right = vel_angular_right*0.03; // La velocidad lineal esta en m/s, se obtiene al multiplicar la velocidad angular por el radio de la rueda (3cm)
+
+  return vel_lineal_right;
+}
+
+
+inline void ActuadorLeft(float u){
+  byte pwm; //valor de PWM a aplicar en el pin digital correspondiente. Recordar que las salidas
+            //PWM son de 8-bit en el Arduino Nano.
+  
+  if (u >= 0.0){ //voltaje medio de alimentacion del motor positivo (aceleracion)
+    if (u!=0.0 && u<ZONA_MUERTA) u = ZONA_MUERTA;
+    if (u > TENSION_MAX) u = TENSION_MAX;
+    //Mapeo de la accion de control (voltaje) a PWM de 8 bits
+    pwm = (byte)( (u/TENSION_MAX)*255.0 ); //representa el duty de la señal PWM
+    //Aplicacion de la accion de control (PWM)
+    digitalWrite(PIN1_MOTOR_LEFT, LOW);
+    analogWrite(PIN2_MOTOR_LEFT,  pwm);
+  } else { //voltaje medio de alimentacion del motor negativo (desaceleracion)
+    u = abs(u);
+    if (u < ZONA_MUERTA) u = ZONA_MUERTA;
+    if (u > TENSION_MAX) u = TENSION_MAX;
+    //Mapeo de la accion de control (voltaje) a PWM de 8 bits
+    pwm = (byte)( (u/TENSION_MAX)*255.0 ); //representa el duty de la señal PWM
+    //Aplicacion de la accion de control (PWM)
+    digitalWrite(PIN2_MOTOR_LEFT, LOW);
+    analogWrite(PIN1_MOTOR_LEFT,  pwm);
+  }
+}
+
+
+inline void ActuadorRight(float u){
+  byte pwm; //valor de PWM a aplicar en el pin digital correspondiente. Recordar que las
+            //salidas PWM son de 8-bit en el Arduino Nano.
+
+  if (u >= 0.0){ //voltaje medio de alimentacion del motor positivo (aceleracion)
+    if (u!=0.0 && u<ZONA_MUERTA) u = ZONA_MUERTA;
+    if (u > TENSION_MAX) u = TENSION_MAX;
+    //Mapeo de la accion de control (voltaje) a PWM de 8 bits
+    pwm = (byte)( (u/TENSION_MAX)*255.0 ); //representa el duty de la señal PWM
+    //Aplicacion de la accion de control (PWM)
+    digitalWrite(PIN1_MOTOR_RIGHT, LOW);
+    analogWrite(PIN2_MOTOR_RIGHT,  pwm);
+  } else { //voltaje medio de alimentacion del motor negativo (desaceleracion)
+    u = abs(u);
+    if (u < ZONA_MUERTA) u = ZONA_MUERTA;
+    if (u > TENSION_MAX) u = TENSION_MAX;
+    //Mapeo de la accion de control (voltaje) a PWM de 8 bits
+    pwm = (byte)( (u/TENSION_MAX)*255.0 ); //representa el duty de la señal PWM
+    //Aplicacion de la accion de control (PWM)
+    digitalWrite(PIN2_MOTOR_RIGHT, LOW);
+    analogWrite(PIN1_MOTOR_RIGHT,  pwm);
+  }
+}
+
+
+inline void ControlMotorLeft(float r){ //r: velocidad lineal de referencia (m/s)
+  //Variables relacionadas con el lazo de control L
+  float y; //velocidad lineal sensada (m/s)
+  float e; //error de velocidad (m/s)
+  float u; //accion de control, voltaje medio a aplicar en el motor, que se materializa como una señal PWM que
+                          //se aplica al driver del motor
+  float integral; //termino integral de la ecuacion del PID
+  float deriv; //termino derivativo de la ecuacion del PID
+  static float y1 = 0.0, e1 = 0.0, u1 = 0.0; //y1 = y(k-1); e1=e(k-1); u1 = u(k-1), u1 solo se utiliza para graficar la accion de control
+  static float integral1 = 0.0, deriv1 = 0.0; //integral1 = integral(k-1); deriv1 = deriv(k-1)
+  
+  ////Lazo L////
+  //Obtencion del valor medido de la salida
+  y = CalcVelLeft();
+
+  //Calculo del error
+  e = r - y;
+
+  if (e > TOLERANCIA){
+    //Calculo de la accion de control del lazo L
+    //Termino integrativo
+    integral = integral1 + kp_left*ki_left*INT_MUESTREO*(e + e1)/2;
+    //Termino derivativo
+    deriv = td_left/(INT_MUESTREO + td_left) * (deriv1 - kp_left*(y - y1));
+    u = kp_left*e + integral + deriv;
+
+    ActuadorLeft(u);
+    
+    //Actualizacion de variables
+    y1 = y;
+    e1 = e;
+    u1 = u;
+    integral1 = integral;
+    deriv1 = deriv;
+  }
+}
+
+
+inline void ControlMotorRight(float r){
+  //Variables relacionadas con el lazo de control R
+  float y; //velocidad lineal sensada (m/s)
+  float e; //error de velocidad (m/s)
+  float u; //accion de control, voltaje medio a aplicar en el motor, que se materializa como una señal PWM que
+                          //se aplica al driver del motor
+  float integral; //termino integral de la ecuacion del PID
+  float deriv; //termino derivativo de la ecuacion del PID
+  static float y1 = 0.0, e1 = 0.0, u1 = 0.0; //y1 = y(k-1); e1=e(k-1); u1 = u(k-1), u1 solo se utiliza para graficar la accion de control
+  static float integral1 = 0.0, deriv1 = 0.0; //integral1 = integral(k-1); deriv1 = deriv(k-1)
+  
+  ////Lazo R////
+  //Obtencion del valor medido de la salida
+  y = CalcVelRight();
+
+  //Calculo del error
+  e = r - y;
+
+  if (e > TOLERANCIA){
+    //Calculo de la accion de control del lazo R
+    //Termino integrativo
+    integral = integral1 + kp_right*ki_right*INT_MUESTREO*(e + e1)/2;
+    //Termino derivativo
+    deriv = td_right/(INT_MUESTREO + td_right) * (deriv1 - kp_right*(y - y1));
+    u = kp_right*e + integral + deriv;
+
+    ActuadorRight(u);
+
+    //Actualizacion de variables
+    y1 = y;
+    e1 = e;
+    u1 = u;
+    integral1 = integral;  
+    deriv1 = deriv;
+  }
 }
 
 
